00b6e7c4ed4e806ee28a3903df481226f55599d3
Asayami <asayamikurashin@gmail.com>
2023-09-29 18:23:17 +0700

    Update Dictionary,java
    remove word_counter

--- dictionary_cmdline/src/Dictionary.java
+++ dictionary_cmdline/src/Dictionary.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
 public class Dictionary {
     protected static ArrayList<Word> Words = new ArrayList<Word>(); // dùng arraylist để quản lí mảng word
-    protected int word_counter;
 }
