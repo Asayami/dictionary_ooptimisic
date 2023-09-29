8e6ff81b6146a26903f8c769a08bfb1baaa60308
Asayami <asayamikurashin@gmail.com>
2023-09-29 22:39:17 +0700

    Test & Update redundant code

--- dictionary_cmdline/src/Dictionary.java
+++ dictionary_cmdline/src/Dictionary.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class Dictionary {
     protected static ArrayList<Word> Words = new ArrayList<Word>(); // dùng arraylist để quản lí mảng word
 }
--- dictionary_cmdline/src/DictionaryCommandline.java
+++ dictionary_cmdline/src/DictionaryCommandline.java
@@ -5,15 +5,13 @@ import java.util.Comparator;
 public class DictionaryCommandline extends DictionaryManagement {
     public void showAllWords() {
         // sắp xếp theo alphabet
-        Words.sort((o1, o2)
-                -> o1.getWord_target().compareTo(
-                o2.getWord_target()));
+        Words.sort(Comparator.comparing(Word::getWord_target));
 
         // out
-        System.out.println(String.format("%-5s %-20s %-15s %-20s %-20s %s", "No", "English","Type", "Vietnamese", "Pronunciation", "Example"));
+        System.out.printf("%-5s %-20s %-15s %-20s %-20s %s%n", "No", "English", "Type", "Vietnamese", "Pronunciation", "Example");
         for (int i = 0; i < Words.size(); i++) {
             Word word_ = Words.get(i);
-            System.out.println(String.format("%-5d %-20s %-15s %-20s %-20s %s", i + 1, word_.getWord_target(),word_.getWord_type(), word_.getWord_explain(), word_.getPronunciation(), word_.getExample()));
+            System.out.printf("%-5d %-20s %-15s %-20s %-20s %s%n", i + 1, word_.getWord_target(), word_.getWord_type(), word_.getWord_explain(), word_.getPronunciation(), word_.getExample());
         }
     }
 
