681b1265b8d0bebad2f55178112ab43a9a5343d4
GP-Sword <138867119+GP-Sword@users.noreply.github.com>
2023-11-22 00:05:43 +0700

    Application icon

--- dictionary_gui/src/gui_package/Start.java
+++ dictionary_gui/src/gui_package/Start.java
@@ -4,18 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image("file:dictionary_gui/src/gui_package/views/images/logo.png");
         FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("views/fxml/main-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Dictionary Ultra Vip Pro Plus+");
+        stage.getIcons().add(icon);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setResizable(false);
         stage.setScene(scene);
